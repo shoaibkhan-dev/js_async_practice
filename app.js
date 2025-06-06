dc33eb7ded707fcc4f1a80620858deffe561031c
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-06 23:56:49 +0500

    add work

--- app.js
+++ app.js
@@ -95,3 +95,13 @@ const promiseOne = new Promise((resolve, reject)=>{
        }
     }, 4000)
 })
+
+async function getAllUsers() {
+    try {
+        const response = await fetch ("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
