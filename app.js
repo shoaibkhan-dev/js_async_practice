9d37bed9993dfa493fde461e561b76f6bb97bedd
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-07 00:52:22 +0500

    add work

--- app.js
+++ app.js
@@ -117,4 +117,14 @@ async function getAllUsers() {
     } catch (error) {
         console.log(error)
     }
-};
+}
+
+async function getUsers(){
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
